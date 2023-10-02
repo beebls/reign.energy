b26537250329683e5abedccbc1e4f4a94c488766
Beebles <102569435+beebls@users.noreply.github.com>
2023-10-01 20:31:05 -0600

    sstg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,3 +22,22 @@
 		<img src="whosippin.gif" alt="i love reign okay who sippin today though" />
 	</div>
 </div>
+<svelte:head>
+	<title>i love reign energy</title>
+	<meta name="title" content="i love reign energy" />
+	<meta name="description" content="regin energ" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://reign.energy/" />
+	<meta property="og:title" content="i love reign energy" />
+	<meta property="og:description" content="regin energ" />
+	<meta property="og:image" content="https://reign.energy/whosippin.gif" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://reign.energy/" />
+	<meta property="twitter:title" content="i love reign energy" />
+	<meta property="twitter:description" content="regin energ" />
+	<meta property="twitter:image" content="https://reign.energy/whosippin.gif" />
+</svelte:head>
--- src/routes/orangedreamsicle/+page.svelte
+++ src/routes/orangedreamsicle/+page.svelte
@@ -22,3 +22,29 @@
 		<img src="orangedreamsicle.gif" alt="orange dreamsicle reign" />
 	</div>
 </div>
+
+<svelte:head>
+	<title>i love orang reign</title>
+	<meta name="title" content="i love reign energy" />
+	<meta name="description" content="regin energ" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://reign.energy/orangedreamsicle" />
+	<meta property="og:title" content="i love reign energy" />
+	<meta
+		property="og:description"
+		content="regin energ REIGN PLEASE RESTOCK ORANGE DREAMSICLE I MISS IT SO MUCH IVE BEEN RATIONING MY LAST 2 CANS PLEASE"
+	/>
+	<meta property="og:image" content="https://reign.energy/orangedreamsicle.gif" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://reign.energy/orangedreamsicle" />
+	<meta property="twitter:title" content="i love reign energy" />
+	<meta
+		property="twitter:description"
+		content="regin energ REIGN PLEASE RESTOCK ORANGE DREAMSICLE I MISS IT SO MUCH IVE BEEN RATIONING MY LAST 2 CANS PLEASE"
+	/>
+	<meta property="twitter:image" content="https://reign.energy/orangedreamsicle.gif" />
+</svelte:head>
