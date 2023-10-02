d44fc0a95a0379998d54155807d2b4e26e8e2591
Beebles <102569435+beebls@users.noreply.github.com>
2023-10-01 20:27:18 -0600

    orange dreamsic

---
+++ src/routes/orangedreamsicle/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let audioRef: HTMLAudioElement;
+	let playing: boolean = false;
+	function play() {
+		audioRef.play();
+		playing = true;
+	}
+	onMount(() => {
+		window.addEventListener('click', play);
+		window.addEventListener('keydown', play);
+	});
+</script>
+
+<div
+	class="flex-grow flex flex-col items-center justify-center"
+	style={!playing ? 'cursor: pointer;' : 'cursor: auto;'}
+>
+	<audio bind:this={audioRef} class="hidden" src="tron.mp3" preload="auto" autoplay />
+	<div class="w-64 h-64">
+		<img src="orangedreamsicle.gif" alt="orange dreamsicle reign" />
+	</div>
+</div>
