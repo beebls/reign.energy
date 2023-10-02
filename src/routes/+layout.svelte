349400e1252e8204a26f6cfcc4e9f42d46032016
Beebles <102569435+beebls@users.noreply.github.com>
2023-10-01 19:41:47 -0600

    meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,23 @@
 <div class="bg-black w-screen min-h-screen flex flex-col">
 	<slot />
 </div>
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>i love reign energy</title>
+	<meta name="title" content="i love reign energy" />
+	<meta name="description" content="regin energ" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://reign.energy/" />
+	<meta property="og:title" content="i love reign energy" />
+	<meta property="og:description" content="regin energ" />
+	<meta property="og:image" content="reign.gif" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://reign.energy/" />
+	<meta property="twitter:title" content="i love reign energy" />
+	<meta property="twitter:description" content="regin energ" />
+	<meta property="twitter:image" content="reign.gif" />
+</svelte:head>
